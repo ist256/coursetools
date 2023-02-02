663770322729d67f6e9e17db08206c346eace933
Michael Fudge <mafudge@syr.edu>
2023-02-02 14:11:56 -0500

    release 1.0.4

--- version.py
+++ version.py
@@ -1,4 +1,5 @@
 history = lambda: [
+    {"ver" : "1.0.4-20230223", "text": "added demo.smartlights" },
     {"ver" : "1.0.3-20221111", "text": "Added display_dataframes() function." },
     {"ver" : "1.0.2-20221111", "text": "Added versioning support." },
     {"ver" : "1.0.1-20221107", "text": "Fixed error in exceptions; problem when netid is an email." },
